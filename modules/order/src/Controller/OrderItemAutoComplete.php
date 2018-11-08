1ef587dcc0c9af055824fad3b0dae0c908be4c81
Shabana Navas <snavas@acromedia.com>
2018-11-08 18:50:52 +0530

    Add functionality to search database if search index is not enabled in autocomplete handler.

--- modules/order/src/Controller/OrderItemAutoComplete.php
+++ modules/order/src/Controller/OrderItemAutoComplete.php
@@ -2,14 +2,14 @@
 
 namespace Drupal\commerce_order\Controller;
 
-use Drupal\search_api\Entity\Index;
+use Drupal\search_api\ParseMode\ParseModePluginManager;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityReferenceSelection\SelectionPluginManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Render\RendererInterface;
 
-use Drupal\search_api\ParseMode\ParseModePluginManager;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -29,18 +29,18 @@ class OrderItemAutoComplete extends ControllerBase {
   protected $entityDisplayRepository;
 
   /**
+   * The entity reference selection handler plugin manager.
+   *
+   * @var \Drupal\Core\Entity\EntityReferenceSelection\SelectionPluginManagerInterface
+   */
+  protected $selectionManager;
+
+  /**
    * The renderer.
    *
    * @var \Drupal\Core\Render\RendererInterface
    */
   protected $renderer;
-
-  /**
-   * The messenger.
-   *
-   * @var \Drupal\Core\Messenger\MessengerInterface
-   */
-  protected $messenger;
 
   /**
    * The parse mode manager.
@@ -50,27 +50,39 @@ class OrderItemAutoComplete extends ControllerBase {
   protected $parseModeManager;
 
   /**
+   * The search api product index.
+   *
+   * @var \Drupal\search_api\Entity\Index
+   */
+  protected $index;
+
+  /**
    * Constructs a new OrderItemAutoComplete object.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
+   * @param \Drupal\Core\Entity\EntityReferenceSelection\SelectionPluginManagerInterface $selection_manager
+   *   The entity reference selection handler plugin manager.
    * @param \Drupal\Core\Render\RendererInterface $renderer
    *   The renderer interface.
-   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-   *   The messenger.
    * @param \Drupal\search_api\ParseMode\ParseModePluginManager $parse_mode_manager
    *   The parse mode manager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory object.
    */
   public function __construct(
     EntityTypeManagerInterface $entity_type_manager,
+    SelectionPluginManagerInterface $selection_manager,
     RendererInterface $renderer,
-    MessengerInterface $messenger,
-    ParseModePluginManager $parse_mode_manager
+    ParseModePluginManager $parse_mode_manager,
+    ConfigFactoryInterface $config_factory
   ) {
     $this->entityTypeManager = $entity_type_manager;
+    $this->selectionManager = $selection_manager;
     $this->renderer = $renderer;
-    $this->messenger = $messenger;
     $this->parseModeManager = $parse_mode_manager;
+    $config = $config_factory->get('commerce_order_item_widget.settings');
+    $this->index = $config->get('product_search_index');
   }
 
   /**
@@ -79,9 +91,10 @@ class OrderItemAutoComplete extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('entity_type.manager'),
+      $container->get('plugin.manager.entity_reference_selection'),
       $container->get('renderer'),
-      $container->get('messenger'),
-      $container->get('plugin.manager.search_api.parse_mode')
+      $container->get('plugin.manager.search_api.parse_mode'),
+      $container->get('config.factory')
     );
   }
 
@@ -94,29 +107,35 @@ class OrderItemAutoComplete extends ControllerBase {
    *   The entity type.
    * @param string $view_mode
    *   The view mode.
-   * @param int $count
+   * @param int $limit
    *   The number of entities to list.
    *
    * @return object
    *   The Json object for auto complete suggestions.
    */
-  public function orderItemAutoCompleteHandler(Request $request, $entity_type, $view_mode, $count) {
-
+  public function orderItemAutoCompleteHandler(
+    Request $request,
+    $entity_type,
+    $view_mode,
+    $limit = 10
+  ) {
     $results = [];
+
     if ($input = $request->query->get('q')) {
-      $suggestions = $this->searchQueryString($input, $count);
-      $view_builder = $this->entityTypeManager->getViewBuilder($entity_type);
-      foreach ($suggestions->getResultItems() as $item) {
-        $data = explode(':', $item->getId());
-        $data = explode('/', $data[1]);
-        $product_variation = $this
-          ->entityTypeManager
-          ->getStorage('commerce_product_variation')
-          ->load($data[1]);
-        $product_render_array = $view_builder->view($product_variation, $view_mode, $product_variation->language()->getId());
+      $product_variations = $this->searchQueryString($entity_type, $input, $limit);
+
+      foreach ($product_variations as $product_variation) {
+        $view_builder = $this->entityTypeManager->getViewBuilder($entity_type);
+        $product_render_array = $view_builder->view(
+          $product_variation,
+          $view_mode,
+          $product_variation->language()->getId()
+        );
         $results[] = [
           'value' => $product_variation->id(),
-          'label' => '<div class="commerce-order-autocomplete-results">' . $this->renderer->renderPlain($product_render_array) . '</div>',
+          'label' => '<div class="commerce-order-autocomplete-results">'
+          . $this->renderer->renderPlain($product_render_array)
+          . '</div>',
         ];
       }
     }
@@ -125,31 +144,60 @@ class OrderItemAutoComplete extends ControllerBase {
   }
 
   /**
-   * Helper function for searching the product.
+   * Helper function for searching for matching products.
+   *
+   * @param string $entity_type
+   *   The entity type.
+   * @param string $string
+   *   The Query string.
+   * @param int $limit
+   *   The count of items to return.
+   *
+   * @return array
+   *   An array of loaded product variations.
+   */
+  public function searchQueryString($entity_type, $string, $limit) {
+    $product_variations = [];
+
+    // If no search_api product index is configured.
+    if (!$this->index) {
+      $suggestions = $this->searchProducts($entity_type, $string, $limit);
+      foreach ($suggestions as $product_variation_id) {
+        $product_variations[] = $this
+          ->entityTypeManager
+          ->getStorage('commerce_product_variation')
+          ->load($product_variation_id);
+      }
+    }
+    // Else, let's search the search_api product index.
+    else {
+      $suggestions = $this->searchProductIndex($string, $limit);
+      foreach ($suggestions->getResultItems() as $item) {
+        $data = explode(':', $item->getId());
+        $data = explode('/', $data[1]);
+        $product_variations[] = $this
+          ->entityTypeManager
+          ->getStorage('commerce_product_variation')
+          ->load($data[1]);
+      }
+    }
+
+    return $product_variations;
+  }
+
+  /**
+   * Helper function for searching for matches in the search api product index.
    *
    * @param string $string
    *   The Query string.
-   * @param int $count
+   * @param int $limit
    *   The count of items to return.
    *
    * @return mixed
    *   The query search result.
    */
-  public function searchQueryString($string, $count) {
-    $config = $this->config('commerce_order_item_widget.settings');
-
-    $index = Index::load($config->get('product_search_index'));
-
-    if (!isset($index)) {
-      $this
-        ->messenger
-        ->addMessage(
-          $this->t('You must configure a Search API index to search against.')
-      );
-      return NULL;
-    }
-
-    $query = $index->query();
+  public function searchProductIndex($string, $limit) {
+    $query = $this->index->query();
 
     $parse_mode = $this->parseModeManager->createInstance('direct');
     $parse_mode->setConjunction('OR');
@@ -158,9 +206,47 @@ class OrderItemAutoComplete extends ControllerBase {
     // Set fulltext search keywords and fields.
     $query->keys($string);
 
-    $query->range(0, $count);
+    $query->range(0, $limit);
 
     $results = $query->execute();
+
+    return $results;
+  }
+
+  /**
+   * Helper function for searching for products in the database.
+   *
+   * @param string $entity_type
+   *   The entity type.
+   * @param string $string
+   *   The Query string.
+   * @param int $limit
+   *   The count of items to return.
+   *
+   * @return array
+   *   An array of product variation IDs if we have matches.
+   */
+  public function searchProducts($entity_type, $string, $limit) {
+    $results = [];
+
+    $options = [
+      'target_type' => $entity_type,
+      'handler' => 'default',
+    ];
+    $handler = $this->selectionManager->getInstance($options);
+
+    if (isset($string)) {
+      // Get an array of matching entities.
+      $match_operator = !empty($selection_settings['match_operator']) ? $selection_settings['match_operator'] : 'CONTAINS';
+      $entity_labels = $handler->getReferenceableEntities($string, $match_operator, $limit);
+
+      // Loop through the entities and convert them into autocomplete output.
+      foreach ($entity_labels as $values) {
+        foreach ($values as $entity_id => $label) {
+          $results[] = $entity_id;
+        }
+      }
+    }
 
     return $results;
   }
